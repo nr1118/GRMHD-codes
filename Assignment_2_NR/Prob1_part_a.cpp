ddb8798896275f838a1c616d42a3059f7eda05f4
Nathan Rutherford <76955751+nr1118@users.noreply.github.com>
2022-02-09 12:58:50 -0500

    Add files via upload

--- Assignment_2_NR/Prob1_part_a.cpp
+++ Assignment_2_NR/Prob1_part_a.cpp
@@ -8,6 +8,7 @@ Mesh::Mesh(int n, std::vector <int> Ext):dim(n),Extents(Ext)
 	if (Extents.size() != n)
 	{
 		std::cout << "ERROR: Need the vector to be of size "<< n <<"\n";
+		exit(1);
 	}
 }
 int Mesh::Get_dim() const //defines what the Get_dim() function returns
@@ -20,6 +21,8 @@ std::vector<int>Mesh::Get_grid_pts()  //this function should return the number o
 	return Extents;
 }
 
+
+
 int Mesh::Get_total_grid_pts() const //This functions simply just sums up the number of points per dimension
 {
 	int x = 0;
@@ -27,6 +30,15 @@ int Mesh::Get_total_grid_pts() const //This functions simply just sums up the nu
 		x = x + Extents[i];
 	}
 	return x;
+}
+const std::vector <double>& Data_Mesh<double>::Get_Const_Data() const
+{
+	return Const_Data; // I tried using &Const_Data and I get an error saying "no suitable constructor exists, but it works without the pointer.
+}
+
+std::vector <double>& Data_Mesh<double>::Get_Mod_Data()
+{
+	return Mod_Data; 
 }
 
 int main()
@@ -36,15 +48,28 @@ int main()
 
 
 
-	Mesh mymesh(n, Ext); // initializes the object of the class Mesh called mymesh
+	Mesh mymesh(n, Ext); //creates an object of the class Mesh and this will be used call anything within the public part of Mesh.
 
-
-
-	std::cout << "The total number of grid points are: " << mymesh.Get_total_grid_pts() << "\n";
-
-	for (int i = 0; i < n; i++) {
-		std::cout << mymesh.Get_grid_pts()[i] << "," << i+1 << "\n";
+	Data_Mesh<double> mDm(n, Ext); //creates an object of the class Data_Mesh and specifies a type since we did not when making the template
+	
+	
+	std::vector<double> Data = mDm.Get_Const_Data();
+	Data = { 1.0 ,2,4,3};
+	
+	for (double x : Data) // I feel like this is not what we are looking for since this loop just print whatever is in data and I am unsure how see what is in Const_Data
+	{
+		std::cout << x;
 	}
+	
+
+	//std::cout << "The total number of grid points are: " << mymesh.Get_total_grid_pts() << "\n";
+
+	//for (int i = 0; i < n; i++) {
+		//std::cout << mymesh.Get_grid_pts()[i] << "," << i+1 << "\n";
+	//}
 
 	return 0;
 }
+
+//potentially useful thing here 
+// Data_Mesh::Data_Mesh(): Mesh(n,Ext)  //this creates a function from the inherited class Data_Mesh called Data_Mesh and it has acess to the Mesh class
